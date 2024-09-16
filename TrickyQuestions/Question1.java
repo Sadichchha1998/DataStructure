52a121367adfcb3cc98a03fa7ca987eff0be0620
Vozon <sadichchha15@gmail.com>
2024-09-16 16:26:54 +0530

    trickeyQuestion1

---
+++ TrickyQuestions/Question1.java
@@ -0,0 +1,12 @@
+public class Question1 {
+    public void sum(int a, int b) {
+        System.out.println(a + b);
+    }
+    public int sum(int a, int b) {
+      return a+b;
+    }
+    public  static  void main(String[] args){}
+    Question1 q1= new Question1();
+    q1.sum(10,20);
+    System.out.println(10,20);
+}
