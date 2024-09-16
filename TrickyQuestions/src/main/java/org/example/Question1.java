0652e8b57fe7e2999638f396e8f597b673fbe31c
Vozon <sadichchha15@gmail.com>
2024-09-16 17:26:13 +0530

    update

--- TrickyQuestions/src/main/java/org/example/Question1.java
+++ TrickyQuestions/src/main/java/org/example/Question1.java
@@ -1,16 +1,17 @@
 package org.example;
 
 public class Question1 {
-    public void sum(int a, int b) {
-        System.out.println(a + b);
-    }
+//    public void sum(int a, int b) {
+//        System.out.println(a + b);
+//    }
     public int sum(int a, int b) {
         return a+b;
     }
     //it give us error because of here we are using we method with same name as a sum and same type and same number of the parameter is not allowd in the java
-    // here the program is not compiled it give us an compile time error.
-    public  static  void main(String[] args){}
-    Question1 q1= new Question1();
-    q1.sum(10,20);
-    System.out.println(10,20);
+    // here the program is not compiled it give us a compile time error.
+    public  static  void main(String[] args) {
+        Question1 q1 = new Question1();
+        q1.sum(10, 20);
+        System.out.println(q1.sum(10,20));
+    }
 }
