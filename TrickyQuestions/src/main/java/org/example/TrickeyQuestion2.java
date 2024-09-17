2f46e5c5b3af8a19b9448065724ccb2883150554
Sadichchha1998 <sadichchha15@gmail.com>
2024-09-17 23:28:38 +0530

    question2

--- TrickyQuestions/src/main/java/org/example/TrickeyQuestion2.java
+++ TrickyQuestions/src/main/java/org/example/TrickeyQuestion2.java
@@ -1,5 +1,29 @@
 package org.example;
 
 public class TrickeyQuestion2 {
+    int count;
 
+    void Test() {
+        count++;
+    }
+
+    public void PrintNumber() {
+        System.out.println(count);
+    }
+
+    public class Question2 {
+        public static void main(String[] args) {
+        }
+
+        TrickeyQuestion2 d1 = new TrickeyQuestion2();
+        TrickeyQuestion2 d2 = new TrickeyQuestion2();
+        TrickeyQuestion2 d3 = new TrickeyQuestion2();
+//    d1.TrickeyQuestion2();
+//      d2.TrickeyQuestion2();
+//        d3.TrickeyQuestion2();
+
+
+// this prgram won't be compiled
+
+    }
 }
