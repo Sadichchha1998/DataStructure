d20ec140b39c449536f27c7ede3d8ec9f3addeda
Sadichchha1998 <sadichchha15@gmail.com>
2024-09-21 23:07:40 +0530

    Trickey Question

---
+++ TrickyQuestions/src/main/java/org/example/TrickeyQuestion16.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class TrickeyQuestion16 {
+    static int data[];
+    public static void main(String[] args) {
+     System.out.println(data.length);
+    }
+    // it will give null pointer exception because of we do not insilized the data
+}
