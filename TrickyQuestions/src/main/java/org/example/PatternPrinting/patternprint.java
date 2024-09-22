0a9ceaef15411fe00c58f668912a1abe867f85de
Sadichchha1998 <sadichchha15@gmail.com>
2024-09-22 22:22:05 +0530

    pattern

--- TrickyQuestions/src/main/java/org/example/PatternPrinting/patternprint.java
+++ TrickyQuestions/src/main/java/org/example/PatternPrinting/patternprint.java
@@ -1,4 +1,7 @@
 package org.example.PatternPrinting;
+
+import java.util.Scanner;
+
 /*
 Description
 You are given a number stored in a variable with the name N
@@ -12,5 +15,17 @@ If the value stored in N = 4 then the pattern required will be
 public class patternprint {
     public static void main(String[] args) {
 
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Entre number to print this pattern");
+        int n= sc.nextInt();
+
+        for (int i=0;i<=n;i++){
+            for (int j=0;j<=i;j++) {
+
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
