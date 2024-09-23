5748b022f0f2659203c60f82cb5101b62800c3f7
Sadichchha1998 <sadichchha15@gmail.com>
2024-09-23 23:44:04 +0530

    stream

---
+++ TrickyQuestions/src/main/java/org/example/String/DetectPalindrome.java
@@ -0,0 +1,7 @@
+package org.example.String;
+
+public class DetectPalindrome {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ TrickyQuestions/src/main/java/org/example/String/PrintCharacterInReverseOrder.java
@@ -0,0 +1,14 @@
+package org.example.String;
+
+import java.util.Scanner;
+
+public class PrintCharacterInReverseOrder {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str= sc.nextLine();
+
+        for(int i=str.length()-1;i>=0;i--){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
