3395f1f6ecb5ff3e51bd297cdea93690799c6061
Sadichchha1998 <sadichchha15@gmail.com>
2024-09-28 21:38:44 +0530

    Such pair

---
+++ TrickyQuestions/src/main/java/org/example/Arrays/CountSuchAPair.java
@@ -0,0 +1,24 @@
+package org.example.Arrays;
+/*
+Description
+You are given an array A of N integers along with a target integer K .
+Your task is to find out the number of pairs of integers present in the array,
+whose sum is equal to the target value K
+Input Format :
+First line contains 2 integers: N and K.
+Second line contains N space separated integers which are the elements of the array.
+Constraints :
+1 N 100
+Output
+Print the count of such pairs whose sum is K.
+Smaple Input:
+5 9
+3 0 6 2 7
+Sample Output
+2
+ */
+public class CountSuchAPair {
+    public static void main(String[] args) {
+
+    }
+}
